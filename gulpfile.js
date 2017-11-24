a7fec702b3586b971ae2870452019f70924d0db3
Braden Marshall <braden1996@hotmail.co.uk>
2017-11-24 23:13:35 +0100

    Add initial package.json and gulpfile.js with TypeSript

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
+var tsify = require('tsify');
+
+function compile(watch) {
+  var bundler = watchify(
+    browserify('./src/index.ts', { debug: true })
+      .plugin(tsify, { target: 'es6' })
+      .transform(babelify, { extensions: [ '.ts' ] })
+  );
+
+  function rebundle() {
+    bundler.bundle()
+      .on('error', function(err) { console.error(err); this.emit('end'); })
+      .pipe(source('build.js'))
+      .pipe(buffer())
+      .pipe(gulp.dest('./build'));
+  }
+
+  if (watch) {
+    bundler.on('update', function() {
+      console.log('-> bundling...');
+      rebundle();
+    });
+  }
+
+  rebundle();
+}
+
+function watch() {
+  return compile(true);
+};
+
+gulp.task('build', function() { return compile(); });
+gulp.task('watch', function() { return watch(); });
+
+gulp.task('default', ['watch']);
