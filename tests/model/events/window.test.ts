7e08aa09d6189335a403a04ceb7cfe146ef36bf9
Braden Marshall <braden1996@hotmail.co.uk>
2017-12-22 17:32:07 +0100

    Add unit-tests for window events.

---
+++ tests/model/events/window.test.ts
@@ -0,0 +1,87 @@
+import SyncedFrame from 'model/frames/SyncedFrame';
+import WindowNode, { ManagedWindow } from 'model/nodes/WindowNode';
+import TileNode from 'model/nodes/TileNode';
+import { TestManagedWindow } from '../nodes/WindowNode.test';
+import {
+  windowAdd,
+  windowRemoveFromParent,
+  windowMinimise,
+  windowUnminimise
+} from 'model/events/window';
+import QueryModel, { ScreenModels } from 'model/utils/QueryModel';
+
+describe('Window events', () => {
+  describe('windowAdd()', () => {
+    it('should create new WindowNode for the given ManagedWindow', () => {
+      const rootFrameRect = <Rectangle>{ x: 0, y: 0, width: 100, height: 100 };
+      const rootSyncedFrame = new SyncedFrame(rootFrameRect);
+      const rootNode = new TileNode(rootSyncedFrame);
+
+      const managedWindow = new TestManagedWindow('Pong');
+      windowAdd(rootNode, managedWindow);
+
+      const children = rootNode.getChildren();
+      expect(children).toHaveLength(1);
+      expect(QueryModel(rootNode).get(managedWindow)).toBeDefined();
+      expect(QueryModel(rootNode).get(managedWindow)).toBe(children[0]);
+    });
+  });
+
+  describe('windowRemoveFromParent()', () => {
+    it('should remove a WindowNode given its ManagedWindow', () => {
+      const rootFrameRect = <Rectangle>{ x: 0, y: 0, width: 100, height: 100 };
+      const rootSyncedFrame = new SyncedFrame(rootFrameRect);
+      const rootNode = new TileNode(rootSyncedFrame);
+
+      const managedWindow = new TestManagedWindow('Pong');
+      windowAdd(rootNode, managedWindow);
+
+      const models = <ScreenModels>{
+        '1': rootNode,
+      };
+
+      windowRemoveFromParent(models, managedWindow);
+      const children = rootNode.getChildren();
+      expect(children).toHaveLength(0);
+    });
+  });
+
+  describe('windowMinimise()', () => {
+    it('should hide a window a WindowNode given its ManagedWindow', () => {
+      const rootFrameRect = <Rectangle>{ x: 0, y: 0, width: 100, height: 100 };
+      const rootSyncedFrame = new SyncedFrame(rootFrameRect);
+      const rootNode = new TileNode(rootSyncedFrame);
+
+      const managedWindow = new TestManagedWindow('Pong');
+      windowAdd(rootNode, managedWindow);
+
+      const models = <ScreenModels>{
+        '1': rootNode,
+      };
+
+      windowMinimise(models, managedWindow);
+      expect(QueryModel(rootNode).get(managedWindow)!.hidden).toBeTruthy();
+    });
+  });
+
+  describe('windowUninimise()', () => {
+    it('should unhide a window a WindowNode given its ManagedWindow', () => {
+      const rootFrameRect = <Rectangle>{ x: 0, y: 0, width: 100, height: 100 };
+      const rootSyncedFrame = new SyncedFrame(rootFrameRect);
+      const rootNode = new TileNode(rootSyncedFrame);
+
+      const managedWindow = new TestManagedWindow('Pong');
+      windowAdd(rootNode, managedWindow);
+
+      const models = <ScreenModels>{
+        '1': rootNode,
+      };
+
+      windowMinimise(models, managedWindow);
+      expect(QueryModel(rootNode).get(managedWindow)!.hidden).toBeTruthy();
+
+      windowUnminimise(models, managedWindow);
+      expect(QueryModel(rootNode).get(managedWindow)!.hidden).toBeFalsy();
+    });
+  });
+});
