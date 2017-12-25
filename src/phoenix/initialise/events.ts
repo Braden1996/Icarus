3dfe553fd8e46e78e1691d1641999e4586ee5796
Braden Marshall <braden1996@hotmail.co.uk>
2017-12-25 21:34:06 +0000

    Added useful debug information for Phoenix Window and App types.

--- src/phoenix/initialise/events.ts
+++ src/phoenix/initialise/events.ts
@@ -1,3 +1,4 @@
+import CONFIG from 'config';
 import ContainerNode from "model/nodes/ContainerNode";
 import {
   windowAdd,
@@ -14,18 +15,22 @@ import { getFocussedScreen } from '../utils/helpers';
 function registerAllEvents(models: ScreenModels) {
   return {
     minimiseWindow: Event.on('windowDidMinimise', (theWindow) => {
+      CONFIG.DEBUG('windowDidMinimise', theWindow);
       const managedWindow = new PhoenixManagedWindow(theWindow);
       if (managedWindow.isValid()) windowMinimise(models, managedWindow);
     }),
     unminimiseWindow: Event.on('windowDidUnminimise', (theWindow) => {
+      CONFIG.DEBUG('windowDidUnminimise', theWindow);
       const managedWindow = new PhoenixManagedWindow(theWindow);
       if (managedWindow.isValid()) windowUnminimise(models, managedWindow);
     }),
     closeWindowRemoveFromLayout: Event.on('windowDidClose', (theWindow) => {
+      CONFIG.DEBUG('windowDidClose', theWindow);
       const managedWindow = new PhoenixManagedWindow(theWindow);
       if (managedWindow.isValid()) windowRemoveFromParent(models, managedWindow);
     }),
     openWindowAddToLayout: Event.on('windowDidOpen', (theWindow) => {
+      CONFIG.DEBUG('windowDidOpen', theWindow);
       const currentScreen = getFocussedScreen(models);
       if (!currentScreen) return;
       const managedWindow = new PhoenixManagedWindow(theWindow);
--- src/phoenix/utils/debug.ts
+++ src/phoenix/utils/debug.ts
@@ -1,6 +1,42 @@
 import CONFIG from 'config';
+import { isApp, isWindow } from './typeGuards';
 
-function mapVariable(someVariable: any): any {
+function mapVariable(someVariable: any, depth = Infinity): any {
+  if (depth < 0) { return 'MAX DEPTH'}
+
+  if (isApp(someVariable)) {
+    const nextDepth = depth === Infinity ? 0 : depth - 1;
+    return {
+      type: 'App',
+      name: someVariable.name(),
+      hash: someVariable.hash(),
+      isActive: someVariable.isActive(),
+      isHidden: someVariable.isHidden(),
+      isTerminated: someVariable.isTerminated(),
+      windows: someVariable.windows()
+        .map(w => mapVariable(w, nextDepth)),
+    }
+  } else if (isWindow(someVariable)) {
+    const nextDepth = depth === Infinity ? 1 : depth - 1;
+    const app = mapVariable(someVariable.app(), nextDepth);
+    if (app instanceof Object) {
+      app.otherWindows = app.windows
+        .filter((w: any) => w.hash !== someVariable.hash());
+      delete app.windows;
+    }
+
+    return {
+      type: 'Window',
+      title: someVariable.title(),
+      hash: someVariable.hash(),
+      isFullScreen: someVariable.isFullScreen(),
+      isMain: someVariable.isMain(),
+      isMinimized: someVariable.isMinimized(),
+      isNormal: someVariable.isNormal(),
+      isVisible: someVariable.isVisible(),
+      app
+    };
+  }
   return someVariable;
 }
 
