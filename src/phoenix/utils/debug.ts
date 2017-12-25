80bd421b8fbd7df418e76a6e1ef747e528e20c52
Braden Marshall <braden1996@hotmail.co.uk>
2017-12-25 23:48:16 +0000

    Recursively map variables to their debug equivalent.

--- src/phoenix/utils/debug.ts
+++ src/phoenix/utils/debug.ts
@@ -2,42 +2,50 @@ import CONFIG from 'config';
 import { isApp, isWindow } from './typeGuards';
 
 function mapVariable(someVariable: any, depth = Infinity): any {
-  if (depth < 0) { return 'MAX DEPTH'}
+  if (!(someVariable instanceof Object)) {
+    return someVariable;
+  } else if (depth < 0) {
+    return someVariable.toString();
+  } else {
+    if (isApp(someVariable)) {
+      return mapVariable({
+        type: 'App',
+        name: someVariable.name(),
+        hash: someVariable.hash(),
+        isActive: someVariable.isActive(),
+        isHidden: someVariable.isHidden(),
+        isTerminated: someVariable.isTerminated(),
+        windows: someVariable.windows(),
+      },  depth === Infinity ? 2 : depth);
+    } else if (isWindow(someVariable)) {
+      const debugWindow = mapVariable({
+        type: 'Window',
+        title: someVariable.title(),
+        hash: someVariable.hash(),
+        isFullScreen: someVariable.isFullScreen(),
+        isMain: someVariable.isMain(),
+        isMinimized: someVariable.isMinimized(),
+        isNormal: someVariable.isNormal(),
+        isVisible: someVariable.isVisible(),
+        app: someVariable.app(),
+      }, depth === Infinity ? 3 : depth);
 
-  if (isApp(someVariable)) {
-    const nextDepth = depth === Infinity ? 0 : depth - 1;
-    return {
-      type: 'App',
-      name: someVariable.name(),
-      hash: someVariable.hash(),
-      isActive: someVariable.isActive(),
-      isHidden: someVariable.isHidden(),
-      isTerminated: someVariable.isTerminated(),
-      windows: someVariable.windows()
-        .map(w => mapVariable(w, nextDepth)),
+      if (debugWindow.app && debugWindow.app.windows instanceof Array) {
+        debugWindow.app.otherWindows = debugWindow.app.windows
+          .filter((w: any) => w.hash !== someVariable.hash());
+        delete debugWindow.app.windows;
+      }
+      return debugWindow;
+    } else if (someVariable instanceof Array) {
+      return someVariable.map(v => mapVariable(v, depth - 1));
+    } else if (someVariable.toJSON) {
+      return mapVariable(someVariable.toJSON(), depth - 1);
+    } else if (someVariable.constructor === Object) {
+      return Object.assign({}, ...Object.keys(someVariable).map(
+        k => ({[k]: mapVariable(someVariable[k], depth - 1)})
+      ));
     }
-  } else if (isWindow(someVariable)) {
-    const nextDepth = depth === Infinity ? 1 : depth - 1;
-    const app = mapVariable(someVariable.app(), nextDepth);
-    if (app instanceof Object) {
-      app.otherWindows = app.windows
-        .filter((w: any) => w.hash !== someVariable.hash());
-      delete app.windows;
-    }
-
-    return {
-      type: 'Window',
-      title: someVariable.title(),
-      hash: someVariable.hash(),
-      isFullScreen: someVariable.isFullScreen(),
-      isMain: someVariable.isMain(),
-      isMinimized: someVariable.isMinimized(),
-      isNormal: someVariable.isNormal(),
-      isVisible: someVariable.isVisible(),
-      app
-    };
   }
-  return someVariable;
 }
 
 function variableToString(someVariable: any): string {
